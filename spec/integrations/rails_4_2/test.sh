f394435df7d70fece15b858efe93af4bff8a9021
Grant Petersen-Speelman <grantp@jobready.com.au>
2018-05-16 16:34:34 +1000

    update test.sh

--- spec/integrations/rails_4_2/test.sh
+++ spec/integrations/rails_4_2/test.sh
@@ -2,23 +2,33 @@
 set -euo pipefail
 
 echo '--- testing rails 4.2'
+
+echo '-- setting environment'
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 RAILS_ENV=test
 BUNDLE_GEMFILE="$DIR/Gemfile"
-
 cd $DIR
-bundle install --retry 2 --jobs 2 --path vendor/bundle
-bin/rails g cypress:install --cypress_folder=test/cypress
 
+echo '-- bundle install'
+bundle install --retry 2 --jobs 2 --path vendor/bundle
+
+echo '-- database setup'
 bin/rake db:create db:schema:load
 
+echo '-- cypress install'
+bin/rails g cypress:install --cypress_folder=test/cypress
+
+
+echo '-- start rails server'
 # make sure the server is not running
 (kill -9 `cat tmp/pids/server.pid` || true )
 
 bin/rails server -p 5002 -d -e test
 sleep 2 # give rails a chance to start up correctly
 
+echo '-- cypress run'
 cd test
 yarn run cypress run
 
+echo '-- stop rails server'
 kill -9 `cat ../tmp/pids/server.pid`
