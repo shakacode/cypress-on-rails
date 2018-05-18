2b0bcea8253fb43dda822e860f581fd9234023af
Grant Petersen-Speelman <grantp@jobready.com.au>
2018-05-18 13:54:39 +1000

    user shift instead of pop for factories

--- lib/generators/cypress/templates/spec/cypress/app_commands/factory_bot.rb
+++ lib/generators/cypress/templates/spec/cypress/app_commands/factory_bot.rb
@@ -1,7 +1,7 @@
 # Example of using factory bot
 if defined?(FactoryBot)
   Array.wrap(command_options).each do |factory_options|
-    factory_method = factory_options.pop
+    factory_method = factory_options.shift
     FactoryBot.public_send(factory_method, *factory_options)
   end
 else
