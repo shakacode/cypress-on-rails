d4391625af97ac2099d7a6f240406259b53d72c3
Grant Petersen-Speelman <grantp@jobready.com.au>
2018-05-20 11:07:58 +1000

    load a warning message if cannot find cypress helper

--- lib/cypress/command_executor.rb
+++ lib/cypress/command_executor.rb
@@ -14,8 +14,11 @@ module Cypress
 
     def self.load_cypress_helper
       cypress_helper_file = "#{configuration.cypress_folder}/cypress_helper"
-      return unless File.exist?("#{cypress_helper_file}.rb")
-      Kernel.require cypress_helper_file
+      if File.exist?("#{cypress_helper_file}.rb")
+        Kernel.require cypress_helper_file
+      else
+        logger.warn "could not find #{cypress_helper_file}.rb"
+      end
     end
 
     def self.logger
