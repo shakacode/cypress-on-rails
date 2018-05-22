232e192be2907c059df07cce979576c6957049cd
Grant Petersen-Speelman <grantp@jobready.com.au>
2018-05-22 17:14:39 +1000

    give better errors from factory_bot

--- lib/generators/cypress/templates/spec/cypress/app_commands/factory_bot.rb
+++ lib/generators/cypress/templates/spec/cypress/app_commands/factory_bot.rb
@@ -2,8 +2,16 @@
 if defined?(FactoryBot)
   Array.wrap(command_options).each do |factory_options|
     factory_method = factory_options.shift
-    FactoryBot.public_send(factory_method, *factory_options)
+    begin
+      logger.debug "running #{factory_method}, #{factory_options}"
+      FactoryBot.public_send(factory_method, *factory_options)
+    rescue => e
+      logger.error "#{e.class}: #{e.message}"
+      logger.error e.backtrace.join("\n")
+      logger.error "#{e.record.inspect}" if e.is_a?(ActiveRecord::RecordInvalid)
+      raise e
+    end
   end
 else
-  logger.warn 'add factory_bot_rails or update factory_bot'
+  logger.warn 'add factory_bot_rails or update factory_bot.rb'
 end
