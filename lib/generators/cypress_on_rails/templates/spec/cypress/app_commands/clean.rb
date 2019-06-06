702d53d91c653f6b562c00ad9552037771838ce3
Michael Yagudaev <michaelyagudaev@gmail.com>
2019-06-06 16:30:44 -0700

    use the cypress on rails logger

--- lib/generators/cypress_on_rails/templates/spec/cypress/app_commands/clean.rb
+++ lib/generators/cypress_on_rails/templates/spec/cypress/app_commands/clean.rb
@@ -1,10 +1,11 @@
+# TODO: this is not covered by unit tests
 if defined?(DatabaseCleaner)
   # cleaning the database using database_cleaner
   DatabaseCleaner.strategy = :truncation
   DatabaseCleaner.clean
 else
-  logger.warn "add database_cleaner or update clean_db"
+  CypressOnRails.logger.error "[CypressOnRails] add database_cleaner or update clean_db"
   Post.delete_all if defined?(Post)
 end
 
-Rails.logger.info "[CypressOnRails] App Cleaned" # used by log_fail.rb
+CypressOnRails.logger.info "[CypressOnRails] Database Cleaned" # used by log_fail.rb
