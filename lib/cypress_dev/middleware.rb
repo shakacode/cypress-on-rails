e772d57f5220613f8f698c7af53291430d063631
Grant <grantspeelman@gmail.com>
2019-06-21 08:19:38 +1000

    support old require statements

---
+++ lib/cypress_dev/middleware.rb
@@ -0,0 +1,2 @@
+warn "cypress_dev is being deprecated, please require \"cypress_on_rails/middleware\" instead"
+require 'cypress_on_rails/middleware'
---
+++ lib/cypress_dev/smart_factory_wrapper.rb
@@ -0,0 +1,2 @@
+warn "cypress_dev is being deprecated, please require \"cypress_on_rails/smart_factory_wrapper\" instead"
+require 'cypress_on_rails/smart_factory_wrapper'
