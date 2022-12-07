74ba03e6ae0bd77de49dbd979f561aac81e9cbdf
Grant Petersen-Speelman <grantspeelman@users.noreply.github.com>
2022-12-08 09:29:32 +1100

    Update update_generator.rb

--- lib/generators/cypress_on_rails/update_generator.rb
+++ lib/generators/cypress_on_rails/update_generator.rb
@@ -5,7 +5,7 @@ module CypressOnRails
 
     def update_generated_files
       template "config/initializers/cypress_on_rails.rb.erb", "config/initializers/cypress_on_rails.rb"
-      template "spec/cypress/cypress_helper.erb.rb", "#{options.cypress_folder}/cypress_helper.rb"
+      template "spec/cypress/cypress_helper.rb.erb", "#{options.cypress_folder}/cypress_helper.rb"
       copy_file "spec/cypress/support/on-rails.js", "#{options.cypress_folder}/support/on-rails.js"
       directory 'spec/cypress/app_commands', "#{options.cypress_folder}/app_commands"
     end
